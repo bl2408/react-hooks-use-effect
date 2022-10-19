e20377e0d683becae839b820e7a0ec770582c34f
Brian Lambert <brianlambert2408@gmail.com>
2022-10-20 10:14:22 +1100

    Completed tasks

--- src/DogPics.js
+++ src/DogPics.js
@@ -6,21 +6,17 @@ function DogPics() {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    console.log("useEffect");
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
         setImages(data.message);
       });
-  });
-
-  console.log("render");
+  }, []);
 
   return (
     <div>
       {images.map((image) => (
-        <img src={image} key={image} />
+        <img src={image} key={image} alt="" />
       ))}
     </div>
   );
